324124ad148a2070a5ef62625a08dcc6b7451153
amrebada <amr.app.engine@gmail.com>
2020-07-11 02:37:40 +0200

    [API] 📝 add swagger docs

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,18 @@ dotenv.config();
 import express, { Application } from "express";
 import config from "./config";
 import bodyParser from "body-parser";
+import swaggerUI from "swagger-ui-express";
 import routes from "./routes";
 import { apiResponse } from "./utils";
 import database from "./database";
+const Documentation = require("../swagger.json");
 
 const app: Application = express();
 
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/v1/explore", swaggerUI.serve, swaggerUI.setup(Documentation));
 
 app.use("/api/v1", routes);
 
@@ -21,7 +25,7 @@ app.all(
   apiResponse.bind(null, () => {
     throw <ErrorType>{
       code: ErrorCodes.NOT_FOUND,
-      message: "Cannot find this resource",
+      message: "Cannot find this resource, see docs on /api/v1/explore",
     };
   })
 );
